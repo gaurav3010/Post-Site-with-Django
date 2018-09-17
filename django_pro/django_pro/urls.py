d987eae4d5770df5ac2c9cad4cfc0023ebb36c3f
gaurav <gauravusadadiya1998@gmail.com>
2018-09-17 23:59:46 +0530

    Making a profile page

--- django_pro/django_pro/urls.py
+++ django_pro/django_pro/urls.py
@@ -18,6 +18,10 @@ from django.contrib.auth import views as auth_view
 from django.urls import path,include
 from users import views as users_views
 
+#static file
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', users_views.register, name = 'register'),
@@ -26,3 +30,7 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
     path('', include("blog.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
