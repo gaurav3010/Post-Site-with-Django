ba6d3ff4a238091ab6e65f5caaf3b2fb85c66a31
gaurav <gauravusadadiya1998@gmail.com>
2018-09-19 23:35:23 +0530

    Create post by login user

--- django_pro/blog/urls.py
+++ django_pro/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 
 ]
--- django_pro/blog/views.py
+++ django_pro/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
 
 # posts = [
 # 	{
@@ -22,6 +24,24 @@ def home(request):
 		'posts':Post.objects.all(),
 	}
 	return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html'
+	context_object_name = 'posts' 
+	ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+	model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+	
 	
 def about(request):
 	return render(request, 'blog/about.html')
